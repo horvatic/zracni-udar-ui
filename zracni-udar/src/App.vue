cbd190b092a5a13c66fa93ea4a8f6c558775f400
horvatic <sbhorvatic@gmail.com>
2021-09-09 04:47:24 +0000

    init template

---
+++ zracni-udar/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Project from './components/Project.vue'
+</script>
+
+<template>
+  <Project/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
